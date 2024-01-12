c41ac8f895916cf4bca9598b52457398d7bc8f85
G <B>
2024-01-12 23:09:16 +0200

    Add a modal form

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -50,26 +50,107 @@ export default function AddPurchaseModal() {
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                   <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 ">
                       <div className="sm:flex sm:items-start">
-                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                          {/* <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
-                        </div>
-                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left ">
                           <Dialog.Title
                             as="h3"
                             className="text-base font-semibold leading-6 text-gray-900"
                           >
-                            Deactivate account
+                            Add a purchase{" "}
                           </Dialog.Title>
                           <div className="mt-2">
-                            <p className="text-sm text-gray-500">
-                              Are you sure you want to deactivate your account?
-                              All of your data will be permanently removed. This
-                              action cannot be undone.
-                            </p>
+                            <div>
+                              <form
+                                className="max-w-md mx-auto bg-white p-8 border border-gray-300 rounded-md"
+                                // ref={ref}
+                                // action={(formData) => {
+                                //   formAction(formData);
+                                //   ref.current?.reset();
+                                // }}
+                              >
+                                <div className="mb-4">
+                                  <label
+                                    htmlFor="title"
+                                    className="block text-sm font-medium text-gray-600"
+                                  >
+                                    Title
+                                  </label>
+                                  <input
+                                    type="text"
+                                    id="title"
+                                    placeholder="Enter title"
+                                    name="title"
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                                    required
+                                  />
+                                </div>
+                                <div className="mb-4">
+                                  <label
+                                    htmlFor="amount"
+                                    className="block text-sm font-medium text-gray-600"
+                                  >
+                                    Amount
+                                  </label>
+                                  <input
+                                    type="text"
+                                    id="amount"
+                                    name="amount"
+                                    placeholder="Enter amount"
+                                    pattern="[0-9]+(\.[0-9]+)?"
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                                    required
+                                  />
+                                </div>
+                                <div className="mb-4">
+                                  <label
+                                    htmlFor="date"
+                                    className="block text-sm font-medium text-gray-600"
+                                  >
+                                    Date
+                                  </label>
+                                  <input
+                                    type="datetime-local"
+                                    id="datetime"
+                                    name="datetime"
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                                    required
+                                  />
+                                </div>
+                                <div className="mb-4">
+                                  <label
+                                    htmlFor="category"
+                                    className="block text-sm font-medium text-gray-600"
+                                  >
+                                    Category
+                                  </label>
+                                  <select
+                                    id="category"
+                                    name="category"
+                                    className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                                    required
+                                  >
+                                    <option value="" disabled>
+                                      Select a category
+                                    </option>
+                                    {/* {categories.map((category) => (
+              <option key={category.id} value={category.categoryName}>
+                {category.categoryName}
+              </option>
+            ))} */}
+                                  </select>
+                                </div>
+                                <div className="mt-4">
+                                  <button
+                                    type="submit"
+                                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                  >
+                                    Submit
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
                           </div>
-                          <div className="mt-2">{/* <ModalForm /> */}</div>
                         </div>
                       </div>
                     </div>
