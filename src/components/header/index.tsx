5276dffa622acff5111c260dc400c72a8b96a262
Siso Ngqolosi <sisoalbert@yahoo.com>
2024-01-31 23:59:44 +0200

    new header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,7 +13,7 @@ const Header: React.FC = () => {
             <div className="h-8 w-8 mr-2 bg-blue-500" />
 
             <h1 className="flex items-center text-1xl font-extrabold dark:text-white">
-              Every Purchase
+              MoneyTracker
               <span className="bg-blue-100 text-blue-800 text-0.1xl font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-2">
                 PRO
               </span>
